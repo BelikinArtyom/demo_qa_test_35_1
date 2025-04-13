38a24ea9854d1595d147683f1384a3de826a7d51
Artyom Belikin <biloplus@yandex.ru>
2025-04-13 22:53:58 +0300

    Правки 3

--- src/test/java/tests/HW3Xpath.java
+++ src/test/java/tests/HW3Xpath.java
@@ -1,4 +1,23 @@
 package PACKAGE_NAME;
 
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeAll;
+
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+
 public class HW3Xpath {
+
+    File file = new File("src/test/imgs/test_img.jpg");
+
+    void cssPath() {
+
+    }
 }
